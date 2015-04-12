1f98d93338690cb6d3ed263ad3b6499be308972d
Marvin Abel <marvinabelmon@gmail.com>
2015-04-12 17:19:32 -0600

    En esta version se muestra la imagen de la cancion en la pagina web, asi como el nombre del artista y el titulo de la cancion

--- js/script.js
+++ js/script.js
@@ -1,16 +1,38 @@
 // Javascript Code.
 
 var list_artist = [];
+/*var song_box = "<div class='row song_box'><div class='col-md-4 image_box'></div><div class='col-md-8 description_box'></div></div>";*/
+var song_box_image = "<div class='row song_box'><div class='col-md-4 image_box'>"; //</div>
+var song_box_info = "</div><div class='col-md-8 description_box'>"; //</div></div>
+var image_tag = "<img src='";
+var description_box = "<div class='col-md-8 description_box'></div>";
 
 var sorting = function(object1, object2) {
     return object2.playcount - object1.playcount;
 };
 
+/*
 var append_songs = function(array) {
+    var count = 1;
+    var position;
     for (i = 0; i < array.length; i++) {
-        $(".body_page").append("<p>"+ array[i]["artist"]["name"]+ array[i]["name"] +"</p>");
+        position = count + i;
+        $(".body_page").append("<p>"+ position.toString()+ array[i]["artist"]["name"]+ array[i]["name"] +"</p>");
     };
 };
+*/
+
+var append_songs = function(array) {
+    var count = 1;
+    var track_position;
+    for (i = 0; i < array.length; i++) {
+        track_position = count + i;
+        if (array[i]["image"]) {
+            $(".body_page").append(song_box_image + image_tag + array[i]["image"][3]["#text"] +"' class='img-thumbnail'>" + song_box_info  + "<p class='song_name'>" + track_position.toString() +"<b>. Track: </b>" + array[i]["name"] + "</p> <p class='artist_name'>  → <b>Artist: </b>"+array[i]["artist"]["name"] + "</p>" + "</div></div>");
+        }
+    };
+};
+
 
 jQuery(document).ready(function($) {
     $.ajax({
@@ -19,9 +41,9 @@ jQuery(document).ready(function($) {
         success : function(parsed_json) {
             var trak_list = parsed_json["tracks"]["track"];
             trak_list.sort(sorting);
-            console.log(trak_list[0]);
-            append_songs(trak_list);
             console.log(trak_list);
+            append_songs(trak_list);
+            console.log(trak_list[0]["image"][2]);
         }
     });
 });
