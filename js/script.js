b78ebc20b36fa82b80623add5f80999190ed072b
AbelMon <marvinabelmon@gmail.com>
2015-04-13 19:42:09 -0600

    En esta version se agregan los botones para el menu de filtros, tambien se cambia la imagen que se visualiza para una cancion que no tiene imagen

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ var song_box_image = "<div class='row song_box'><div class='col-md-4 image_box'>
 var song_box_info = "</div><div class='col-md-8 description_box'>"; //</div></div>
 var image_tag = "<img src='";
 var description_box = "<div class='col-md-8 description_box'></div>";
+var no_image = "images/music.png";
 
 var sorting = function(object1, object2) {
     return object2.playcount - object1.playcount;
@@ -28,8 +29,10 @@ var append_songs = function(array) {
     for (i = 0; i < array.length; i++) {
         track_position = count + i;
         if (array[i]["image"]) {
-            $(".body_page").append(song_box_image + image_tag + array[i]["image"][3]["#text"] +"' class='img-thumbnail'>" + song_box_info  + "<p class='track_position'><b> Position: </b>" + track_position.toString() + "</p><p class='song_name'> <b>Track: </b>" + array[i]["name"] + "</p> <p class='artist_name'><b>Artist: </b>"+array[i]["artist"]["name"] + "</p>" + "<p class='playcount'> <b>Playcount: </b>"+ array[i]["playcount"] + "</p></div></div>");
-        }
+            $(".body_page").append(song_box_image + image_tag + array[i]["image"][3]["#text"] +"' class='img-thumbnail'>" + song_box_info  + "<p class='track_position'><b> Position: </b>" + track_position.toString() + "</p><p class='song_name'> <b>Track: </b>" + array[i]["name"] + "</p> <p class='artist_name'><b>Artist: </b>"+array[i]["artist"]["name"] + "</p>" + "<p class='playcount'> <b>Playcount: </b>"+ array[i]["playcount"] + "</p><p class='duration'><b>Duration: </b>"+ array[i]["duration"] + "</p></div></div>");
+        } else {
+            $(".body_page").append(song_box_image + image_tag + no_image +"' class='img-thumbnail'>" + song_box_info  + "<p class='track_position'><b> Position: </b>" + track_position.toString() + "</p><p class='song_name'> <b>Track: </b>" + array[i]["name"] + "</p> <p class='artist_name'><b>Artist: </b>"+array[i]["artist"]["name"] + "</p>" + "<p class='playcount'> <b>Playcount: </b>"+ array[i]["playcount"] + "</p><p class='duration'><b>Duration: </b>"+ array[i]["duration"] + "</p></div></div>");
+        };
     };
 };
 
